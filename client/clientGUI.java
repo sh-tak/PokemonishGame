9ded048cf7069a241da4abcaed4462e455cead18
yunkai1841 <70383706+yunkai1841@users.noreply.github.com>
2022-05-27 15:37:45 +0900

    select waza test

--- client/clientGUI.java
+++ client/clientGUI.java
@@ -1,41 +1,58 @@
 package client;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
+import java.awt.*;
+import java.io.IOException;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
-import javax.swing.JTextField;
-import javax.swing.border.EmptyBorder;
+import javax.swing.*;
+import javax.swing.tree.*;
+import server.*;
+
 
 public class clientGUI extends JFrame {
-
-	clientGUI() {
+	clientGUI() throws IOException{
 		this.setTitle("Pokemonish Game");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // exit out of the game
 		this.setSize(800, 600);
-		this.setVisible(true);
+		// this.setVisible(true);
+		getContentPane().setLayout(new FlowLayout());
 		JTextField inputArea; //input field 
 		JTextArea freeArea; // output field 
-		Client client = null; // Client object
-		String host = "localhost";
+		// Client client = null; // Client object
+		// String host = "localhost";
 		JTextField hostField, portField;
 		JButton connectBut ,closeBut, quitBut;
 		int port = 28000;
 		Thread thread = null;
 		JScrollPane scrollpane = null;
+		JList wazaList;
 
 		// ImageIcon icon = new ImageIcon("/Users/pokemon.png");
 		// this.setIconImage(icon.getImage());
 		// this.getContentPane().add(new JLabel(icon)); // change color of background
 		//
+		freeArea = new JTextArea("hello world!", 4, 20);
+		getContentPane().add(freeArea);
 
+		ServerThread st = new ServerThread(null, null, Server.generateMonster(), true);
+
+		String[] listData = {"waza1", "waza2", "waza3", "waza4"};
+		wazaList = new JList();
+		wazaList.setListData(st.monster.moveList);
+		getContentPane().add(wazaList);
+		setVisible(true);
+  
+		int selectednumber = wazaList.getSelectedIndex();
+
+		freeArea.setText(Integer.toString(selectednumber));
+		setVisible(true);
 	}
 
 	public static void main(String[] args) {
-		clientGUI frame = new clientGUI();
+		try {
+			clientGUI frame = new clientGUI();
+			
+		} catch (Exception e) {
+			//TODO: handle exception
+		}
 	}
 }
