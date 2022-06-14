6ad0b49786202bf40934b298af6a8455348124f2
yunkai1841 <70383706+yunkai1841@users.noreply.github.com>
2022-06-14 13:56:09 +0900

    update button hp log

--- client/ClientUI.java
+++ client/ClientUI.java
@@ -1,6 +1,6 @@
 package client;
 import java.awt.*;
-// import java.awt.event.*;
+import java.awt.event.*;
 // import java.io.IOException;
 
 import javax.swing.*;
@@ -16,6 +16,14 @@ public class ClientUI extends JFrame{
         client.log("1\n");
         client.log("2\n");
         client.setImage("client/image/a.png");
+        client.setHp(20);
+        client.setButtonAction(new ActionListener(){
+            public void actionPerformed(ActionEvent e) {
+              JOptionPane.showMessageDialog
+              (null, client.getSelectedWaza());
+            }
+        });
+        client.clearLog();
     }
     
     JLabel imageLabel;
@@ -23,6 +31,7 @@ public class ClientUI extends JFrame{
     JList<String> wazaList;
     JButton okButton;
     JTextArea logArea;
+    JProgressBar hpBar;
 
     ClientUI() {
         setTitle("Pokemonish Game");
@@ -31,7 +40,11 @@ public class ClientUI extends JFrame{
         getContentPane().setLayout(new FlowLayout());
         
         imageLabel = new JLabel("no image");
+        imageLabel.setBounds(5, 5, 100, 100);
         statusLabel = new JLabel("wait for status");
+        hpBar = new JProgressBar(1, 100);
+        hpBar.setBounds(0, 0, 100, 5);
+        hpBar.setValue(100);
         String[] initialList = {"a", "b", "c", "d"};
         wazaList = new JList<>(initialList);
         okButton = new JButton("OK");
@@ -41,6 +54,7 @@ public class ClientUI extends JFrame{
         
         getContentPane().add(imageLabel);
         getContentPane().add(statusLabel);
+        getContentPane().add(hpBar);
         getContentPane().add(wazaList);
         getContentPane().add(okButton);
         getContentPane().add(logArea);
@@ -61,14 +75,24 @@ public class ClientUI extends JFrame{
         imageLabel.setText("");
     }
 
-    public void setButtonAction(Action action) {
-        okButton.setAction(action);
+    public void setButtonAction(ActionListener a) {
+        okButton.addActionListener(a);
     }
 
     public void log(String log) {
         logArea.append(log);
     }
+
+    public void clearLog() {
+        logArea.setText(null);
+    }
+
+    public void setHp(int hp){
+        // from 0 to 100 integer
+        hpBar.setValue(hp);
+    }
+
+    public int getSelectedWaza(){
+        return wazaList.getSelectedIndex();
+    }
 }
-© 2022 GitHub, Inc.
-Terms
-Privacy
