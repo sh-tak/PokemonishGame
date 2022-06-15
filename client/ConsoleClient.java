c89549785565bc6e7327b7e8c0510463a7528b41
ShuheiTake <shuhei000@icloud.com>
2022-06-15 22:06:51 +0900

    make ConsoleClient no longer thread because there is no need it should be

--- client/ConsoleClient.java
+++ client/ConsoleClient.java
@@ -2,19 +2,13 @@ package client;
 
 import java.io.IOException;
 
-public class ConsoleClient extends Thread {
-    Client client;
-
+public class ConsoleClient {
     public static void main(String[] args) {
-        ConsoleClient cClient = new ConsoleClient();
-        cClient.run();
-    }
-
-    public void run() {
         try {
+            Client client = new Client();
             client = new Client();
             client.connect();
-            client.recieveAndLog(3); //接続確認応答を受け取る 
+            client.recieveAndLog(3); // 接続確認応答を受け取る
             client.nameInputAndAck();
             client.logging("あなたのモンスターを生成します");
             client.recieveAndLog(10); // 生成したモンスターのデータを受け取る
@@ -22,7 +16,7 @@ public class ConsoleClient extends Thread {
             client.recieveAndLog(2); // 対戦相手を表示
             // 対戦開始
             client.logging("対戦開始");
-            client.initTurn(client.recieve()); //先攻か後攻かを受け取ってターンを初期化
+            client.initTurn(client.recieve()); // 先攻か後攻かを受け取ってターンを初期化
             client.inBattle();
             client.showBattleResult();
         } catch (IOException | InterruptedException e) {
